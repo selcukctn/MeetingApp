99284d9f2c61fc2c2eee39ac9484cfae32fafd11
Harun Selçuk Çetin <56540582+selcukctn@users.noreply.github.com>
2024-08-16 16:07:16 +0300

    ViewBag, ViewData
    Use viewbag and viewdata

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,6 +8,11 @@ namespace MeetingApp.Controllers
     {
         public IActionResult Index()
         {
+            int saat = DateTime.Now.Hour;
+            var selam = saat > 12 ? "İyi Günler" : "Günaydın";
+            //return View(model:selam);
+            //ViewBag.Selam = saat > 12 ? "İyi Günler" : "Günaydın";
+            ViewData["Selam"] = saat > 12 ? "İyi Günler" : "Günaydın";
             return View();
         }
     }
