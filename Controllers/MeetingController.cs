9691afb30f143ad93b8ce836938f53af2d08492a
Harun Selçuk Çetin <56540582+selcukctn@users.noreply.github.com>
2024-08-17 10:59:59 +0300

    Form Validations
    Form Validations

--- Controllers/MeetingController.cs
+++ Controllers/MeetingController.cs
@@ -18,9 +18,14 @@ namespace MeetingApp.Controllers
         public IActionResult Apply(UserInfo model)
         {
             //database
-            Repository.CreateUser(model);
-            ViewBag.UserCount = Repository.Users.Where(i => i.WillAttend == true).Count();
-            return View("Thanks", model);
+            if(ModelState.IsValid){
+                Repository.CreateUser(model);
+                ViewBag.UserCount = Repository.Users.Where(i => i.WillAttend == true).Count();
+                return View("Thanks", model);
+            } else {
+                return View(model);
+            }
+
         }
         public IActionResult List()
         {
--- Models/UserInfo.cs
+++ Models/UserInfo.cs
@@ -1,12 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace MeetingApp.Models
 {
     public class UserInfo
     {
         public int Id {get;set;}
+
+        [Required(ErrorMessage ="Ad alanı zorunludur")]    
         public string? Name {get;set;}
+        
+        [Required(ErrorMessage ="Email alanı zorunludur")]
+        [EmailAddress(ErrorMessage ="Geçerli bir mail adresi giriniz.")]
         public string? Email {get;set;}
+        
+        [Required(ErrorMessage ="Telefon alanı zorunludur")]
         public string? Phone {get;set;}
-        public bool WillAttend {get;set;}
+        
+        [Required(ErrorMessage ="Seçim yapmak zorunludur")]
+        public bool? WillAttend {get;set;}
     }
 
 }
